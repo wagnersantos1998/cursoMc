d3767c117206aee4b2fed4bf455cda355f95a106
Wagner Santos <Wagner_Santos@live.com>
2018-12-21 22:39:34 -0200

    Projeto Criado

---
+++ src/main/java/com/wagnersantos/cursomc/CursoMc1Application.java
@@ -0,0 +1,14 @@
+package com.wagnersantos.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMc1Application.class, args);
+	}
+
+}
+
